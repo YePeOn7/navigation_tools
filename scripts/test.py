9248625ff59c3b5b4352c377953235eec44de7b1
Yohan Prakoso <prakoso.yohan@gmail.com>
2022-05-23 21:33:55 +0800

    remove unused files

--- scripts/test.py
+++ scripts/test.py
@@ -1,20 +1,2 @@
-
-import sys, select, termios, tty
-
-def getKey(key_timeout):
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
-    if rlist:
-        key = sys.stdin.read(1)
-    else:
-        key = ''
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
-
-settings = termios.tcgetattr(sys.stdin)
-while 1:
-    key = getKey(None)
-    print(type(key))
-
-    if (key == '\x03'):
-        break
+#!/usr/bin/env python
+print("test")
